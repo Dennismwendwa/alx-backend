4a1642e7ab83073b4c9856bcec476f4364dc92e6
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-31 08:33:57 +0300

    implemting cache using FIFO

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""This script implements FIFOCache system"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """This is the parent class"""
+    def __init__(self):
+        """Initializes the FIFOCache class"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """This method adds items to the cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = self.queue.pop(0)
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """This method gets items from the cache"""
+        if key is not None:
+            return self.cache_data.get(key)
