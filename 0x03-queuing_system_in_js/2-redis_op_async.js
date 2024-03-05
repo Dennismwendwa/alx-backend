d7a3d2ebe867bc0fb53620431cc151ae5d0ec2f5
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-03-05 22:58:02 +0300

    created redis events using aync

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,39 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+const getAsync = promisify(client.get).bind(client);
+
+async function setNewSchool(schoolName, value, callback) {
+  client.set(schoolName, value, (err, reply) => {
+    if (err) {
+      console.error(`Error setting value for key ${schoolName}: ${err}`);
+    } else {
+      console.log('Reply:', reply);
+    }
+  });
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const reply = await getAsync(schoolName);
+    console.log(reply);
+  } catch (err) {
+    console.log(`Error getting value for key ${schoolName}: ${err}`);
+  }
+}
+
+(async () => {
+  await displaySchoolValue('Holberton');
+  await setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();
